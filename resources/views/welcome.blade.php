471c84214538ae335cbf924945d51d6a7e2dd300
3B232093 <3b232093@gm.student.ncut.edu.tw>
2026-04-06 21:07:43 +0800

    "fix: 修正瀏覽器 tab 顯示為 Laravel 的問題
    - .env APP_NAME 改為 TripFlow
    - 新增 public/favicon.svg（指南針圖示）
    - app/guest/welcome layout 加入 SVG favicon 連結"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>TripFlow - 智慧旅遊規劃平台</title>
-    
+    <link rel="icon" type="image/svg+xml" href="/favicon.svg">
+
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700,800&display=swap" rel="stylesheet" />
     <script src="https://cdn.tailwindcss.com?plugins=typography"></script>
